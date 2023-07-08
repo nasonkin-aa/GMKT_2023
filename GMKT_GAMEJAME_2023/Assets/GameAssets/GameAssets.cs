81f052b2b3ec7d4ac1429468d31e8cb3a3ed5df9
Maxim <ilchaa1998@gmail.com>
2023-07-08 19:37:19 +1000

    add soundManager

---
+++ GMKT_GAMEJAME_2023/Assets/GameAssets/GameAssets.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour {
+    private static GameAssets _i;
+
+    public static GameAssets i {
+        get {
+            if (_i == null) _i = Instantiate(Resources.load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    public AudioClip playerAttack;
+}
