6067362668d430fa909ec0944e1772f7ea85b46c
nasonkin-aa <game-akaynt@mail.ru>
2023-07-10 00:28:01 +1000

    Fix texture

--- GMKT_GAMEJAME_2023/Assets/Scripts/Tiles/WaterTile.cs
+++ GMKT_GAMEJAME_2023/Assets/Scripts/Tiles/WaterTile.cs
@@ -3,6 +3,15 @@ using EffectType;
 
 public class WaterTile : DefaultTile
 {
+    
+    public Sprite newSprite; 
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     public override void GetEffect(GameObject playerProjectile)
     {
         BulletCharacter bulletCharacter = playerProjectile.GetComponent<BulletCharacter>();
@@ -11,6 +20,7 @@ public class WaterTile : DefaultTile
 
         if (EffectTypes.Ice == bulletCharacter.type)
         {
+            spriteRenderer.sprite = newSprite;
             _type = EffectTypes.Ice;
             GetComponent<BoxCollider2D>().enabled = false;
             Destroy(playerProjectile);
